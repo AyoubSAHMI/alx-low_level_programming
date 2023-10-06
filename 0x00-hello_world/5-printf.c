d0540f9cbae17725ad3bb209f8d3662a8c93a628
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-10-05 17:43:10 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/** It's a program where we print
+ * the phrase: with proper grammar, but the outcome is a piece of art,
+ */
+#include<stdio.h>
+int main()
+{
+/* A function that prints this phrase */
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
