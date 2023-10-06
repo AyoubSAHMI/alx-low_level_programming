33f124fae931c15c5006b99bada372875d34f1fd
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-10-05 17:01:45 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * It's a program that print
+ * Programming is like building a multilingual puzzle
+ */
+
+int main()
+{
+  char Mystr[] = "Programming is like building a multilingual puzzle";
+  puts(Mystr);
+  /* writing the string to stdout */
+  return 0;
+}
