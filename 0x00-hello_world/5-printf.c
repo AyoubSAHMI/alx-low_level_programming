3f4b5704c602e8d9ce88f61516e85d0194cd754b
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-10-05 17:52:57 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-/** It's a program where we print
- * the phrase: with proper grammar, but the outcome is a piece of art,
- */
 #include<stdio.h>
-/* A function that prints this phrase */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-/* A function that prints this phrase */
         printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }
