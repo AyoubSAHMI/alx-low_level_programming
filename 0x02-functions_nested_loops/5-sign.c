6340364252f9743079b2a58c63a3f3081d2abe28
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-10-10 17:52:37 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+/**
+ *print_sign - is a function that checks if a number is positive of negative
+ *
+ *@n: number of test
+ *
+ *Return: 1 if number is positive, 0 if it's equal to 0, -1 if it's less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
