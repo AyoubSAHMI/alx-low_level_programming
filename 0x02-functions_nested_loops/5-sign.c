54bf8aac371f79904a201651512e198eb60003ac
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-10-10 18:10:14 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,16 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar(43 + 0);
+		_putchar(43 +'0');
 	}
 	else if (n == 0)
 	{
-		_putchar(0 + 48);
+		_putchar('0' + 48);
 		return (0);
 	}
 	else
 	{
-		_putchar(45 + 0);
+		_putchar(45 + '0');
 		return (-1);
 	}
 }
