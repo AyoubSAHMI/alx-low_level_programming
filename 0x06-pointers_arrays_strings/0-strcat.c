be6ae4067bdeade394296c6fcaf8f945d9e3ce57
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-10-19 16:58:46 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,12 +16,13 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	j = 0;
 
-	for (i = 0 ; src[i] == '\0' ; i++)
+	for (i = 0 ; src[i] != '\0' ; i++)
 	{
-		for (j = 0 ; dest[j] == '\0' ; j++)
+		for (j = 0 ; dest[j] != '\0' ; j++)
 		{
 			dest[j + i] = src[i];
 		}
 	}
+	dest[j + i] = '\0';
 	return (dest);
 }
