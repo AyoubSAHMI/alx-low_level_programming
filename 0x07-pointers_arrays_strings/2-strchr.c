c299a221fbbd7ac73327400987c0695dffeda2b2
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-10-23 17:21:21 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,18 +10,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
-
-	while (s[i] != '\0')
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
-		else
-		{
-			return (0);
-		}
-		i++;
 	}
+	return (0);
 }
