f52c430d727264eb9551b0ed5d645ded1d9fcdc0
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-10-23 17:13:30 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_strchr - a function that returns the pointer of a caracter in a string
+ *@s: the given string
+ *@c: the char we are looking for
+ *Return: a pointer to the first occurence of the char in the string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		else
+		{
+			return (NULL);
+		}
+		i++;
+	}
+}
