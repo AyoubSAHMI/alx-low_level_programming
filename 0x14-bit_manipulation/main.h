8b70eeb74e21384c5e01d253ed320158ba6e4d55
Ayoub SAHMI <ayoubsahmi@gmail.com>
2023-11-23 17:54:24 -0800

    0. 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -13,22 +13,4 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 unsigned int power(unsigned int base, unsigned int exponent);
 
-/**
-*power - a function that returns power of a number
-*@base : the number
-*@exponent: the power
-*Return: the wanted number
-*/
-unsigned int power(unsigned int base, unsigned int exponent)
-{
-unsigned int result;
-unsigned int i;
-result = 1;
-i = 0;
-for (i = 0; i < exponent; ++i)
-{
-result *= base;
-}
-return (result);
-}
 #endif
---
+++ 0x14-bit_manipulation/power.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*power - a function that returns power of a number
+*@base : the number
+*@exponent: the power
+*Return: the wanted number
+*/
+unsigned int power(unsigned int base, unsigned int exponent)
+{
+unsigned int result;
+unsigned int i;
+result = 1;
+i = 0;
+for (i = 0; i < exponent; ++i)
+{
+result *= base;
+}
+return (result);
+}
